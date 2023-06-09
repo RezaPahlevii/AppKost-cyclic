b4fef7ad9197493226c280a59020fcd115dbbdda
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-09 20:00:49 +0700

    + verifyPemilik & adminOnly

--- backend/routes/PemilikRoute.js
+++ backend/routes/PemilikRoute.js
@@ -6,14 +6,15 @@ import {
     updatePemilik,
     deletePemilik
 } from "../controllers/Pemilik.js";
+import { verifyPemilik, adminOnly } from "../middleware/AuthPemilik.js";
 
 const router = express.Router();
 
-router.get('/pemilik-kost', getPemilik);
-router.get('/pemilik-kost/:id', getPemilikById);
-router.post('/pemilik-kost', createPemilik);
-router.patch('/pemilik-kost/:id', updatePemilik);
-router.delete('/pemilik-kost/:id', deletePemilik);
+router.get('/pemilik-kost', verifyPemilik, adminOnly, getPemilik);
+router.get('/pemilik-kost/:id',verifyPemilik, adminOnly, getPemilikById);
+router.post('/pemilik-kost',verifyPemilik, adminOnly, createPemilik);
+router.patch('/pemilik-kost/:id',verifyPemilik, adminOnly, updatePemilik);
+router.delete('/pemilik-kost/:id',verifyPemilik, adminOnly, deletePemilik);
 
 
 export default router;
